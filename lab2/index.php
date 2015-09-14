9eafab98c0628d733f683fe757e2e222b88e01b4
Jon Led <snakers.7@gmail.com>
2015-09-14 18:56:49 +0200

    Lab2 Initial commit

---
+++ lab2/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<HTML>
+<HEAD>
+	<TITLE>Creditos</TITLE>
+	<LINK rel="STYLESHEET" href="default.css" type="text/css">
+</HEAD>
+<BODY>
+	<DIV class="container">
+		<DIV class='title'>
+			<H1>Creditos</h1>
+		</DIV>
+		<DIV class='image'>
+			<img src='img/default_profile.gif' />
+		</DIV>
+		<DIV class='text'>
+		</DIV>
+	</DIV>
+</BODY>
+</HTML>
